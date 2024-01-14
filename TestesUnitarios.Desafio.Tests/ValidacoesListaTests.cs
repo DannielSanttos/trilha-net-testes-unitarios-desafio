41cd6ef6b0637484b9b4f43682440a07a6614874
Daniel Santos <danieldssc2204git config --global user.email danieldssc2204@gmail.com>
2024-01-14 18:40:44 -0300

    desafio teste unitário feito

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using TestesUnitarios.Desafio.Console.Services;
 
 namespace TestesUnitarios.Desafio.Tests;
@@ -44,11 +45,17 @@ public class ValidacoesListaTests
         var numeroParaProcurar = 10;
 
         // Act
+        //lista.Remove(numeroParaProcurar); 
+        var resultado = !_validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
+
 
         // Assert
+        Assert.True(resultado);
+        
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
@@ -56,12 +63,16 @@ public class ValidacoesListaTests
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
-        
+        var resultado = new List<int>(); //{ 10, 14, 16, 18 };
         // Act
+        foreach(int item in lista)
+        {
+            resultado.Add(item*2);
+        };
 
         // Assert
+        Assert.Equal(resultado,resultadoEsperado);
     }
-
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
     {
@@ -71,10 +82,12 @@ public class ValidacoesListaTests
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        int maiorNumero = lista.Max();
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+
+        Assert.Equal(9, maiorNumero);
     }
 
     [Fact]
@@ -90,6 +103,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
